5a55e5f40a3b0648e5644f8fff0e2d27f3dcc6f1
Javier Gomez <javier.gov@outlook.es>
2023-08-26 23:30:17 -0600

    Creando matriz transpuesta

---
+++ src/EjemploMatrizTranspuesta.java
@@ -0,0 +1,35 @@
+public class EjemploMatrizTranspuesta {
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+        System.out.println("Matriz original ");
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Matriz transpuesta ");
+        int auxiliar;
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < i; j++){
+                auxiliar = matriz[i][j];
+                matriz[i][j] = matriz[j][i];
+                matriz[j][i] = auxiliar;
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
