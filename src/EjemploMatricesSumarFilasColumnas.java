708afc7cd420c4d647883c658b96b160555fd846
Javier Gomez <javier.gov@outlook.es>
2023-08-31 22:53:44 -0600

    Suma de elementos de filas y columnas

---
+++ src/EjemploMatricesSumarFilasColumnas.java
@@ -0,0 +1,24 @@
+public class EjemploMatricesSumarFilasColumnas {
+    public static void main(String[] args) {
+        int[][] a = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int sumaFila, sumaColumna;
+
+        for (int i = 0; i < a.length; i++){
+            sumaFila = 0;
+            sumaColumna = 0;
+            for (int j = 0; j < a[i].length; j++){
+                sumaFila += a[i][j];
+                sumaColumna += a[j][i];
+            }
+            System.out.println("Total fila = " + i + " : " + sumaFila);
+            System.out.println("Total columna = " + i + " : " + sumaColumna);
+
+        }
+
+    }
+}
